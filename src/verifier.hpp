1a95548c6c48e568835f39dbc84cca3dc79ff120
Aarav Maloo <aaravmaloo06@gmail.com>
2026-01-09 18:59:48 +0530

    update header file for verifier

--- src/verifier.hpp
+++ src/verifier.hpp
@@ -6,8 +6,15 @@ namespace fastget {
 
 class Verifier {
 public:
+    enum class HashType { SHA256, MD5, SHA1, SHA512 };
+
+    static std::string ComputeHash(const std::string& filename, HashType type);
     static std::string ComputeSHA256(const std::string& filename);
-    static bool Verify(const std::string& filename, const std::string& expected_hash);
+    static std::string ComputeMD5(const std::string& filename);
+    static std::string ComputeSHA1(const std::string& filename);
+    static std::string ComputeSHA512(const std::string& filename);
+
+    static bool Verify(const std::string& filename, const std::string& expected_hash, HashType type = HashType::SHA256);
 };
 
 } // namespace fastget

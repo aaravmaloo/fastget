27646d2ab33b73f6b9806a8b249f0603ce786afe
Aarav Maloo <aaravmaloo06@gmail.com>
2026-02-08 11:42:36 +0530

    Create file_writer.hpp

---
+++ src/file_writer.hpp
@@ -0,0 +1,28 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <mutex>
+#include <vector>
+
+namespace fastget {
+
+class FileWriter {
+public:
+    FileWriter(const std::string& filename);
+    ~FileWriter();
+
+    bool Open();
+    void PreAllocate(size_t size);
+    bool WriteAt(size_t offset, const std::vector<char>& data);
+    void Close();
+
+    bool Exists() const;
+    size_t GetSize() const;
+
+private:
+    std::string filename_;
+    std::fstream file_;
+    std::mutex write_mutex_;
+};
+
+}
